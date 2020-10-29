1f10f419eb9116dd382a4fa6124e9407e3737bb3
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2020-10-28 21:22:35 -0600

    Updated Announcement Cmd | Added Channel Cache

--- src/commands/admin/announcement.js
+++ src/commands/admin/announcement.js
@@ -3,7 +3,9 @@ module.exports = {
         name: "announce",
         category: "admin",
         description: "Announce a message!",
+
         run: async (client, message, args, config, language) => {
+
             var messagetext = args.slice(0).join(' ');
 
             if (!message.member.hasPermission("MANAGE_MESSAGES")) {
@@ -15,15 +17,19 @@ module.exports = {
             }
 
             if (config.announcementschannel && message.member.hasPermission("MANAGE_MESSAGES")) {
-                var announcchannel = message.guild.channels.find(channel => channel.name === `${config.announcementschannel}`) || message.channel;
-                let embed = new RichEmbed()
+
+                var announcchannel = message.guild.channels.cache.find(channel => channel.name === `${config.announcementschannel}`) || message.channel;
+                
+            let embed = new RichEmbed()
                 .setAuthor(`${language.annfrom} ${message.author.username}`)
                 .setColor(`${config.color}`)
                 .setDescription(`${language.sentfrom} ${message.channel.name} by ${message.author.username}`)
                 .addField(`${language.text}`, `${messagetext}`, true)
                 .setTimestamp()
-                announcchannel.send(embed)
-                announcchannel.send(`@everyone`)
+               
+             announcchannel.send(embed)
+             
+              announcchannel.send(`@everyone`)
                
             }
         }
